f95ebfe95396b81b45993bed7b9055d48a26b169
Neejanand Parjapati <110567693+Neejanand47@users.noreply.github.com>
2023-06-24 10:11:49 +0530

    Update Whether API.py

--- Whether API.py
+++ Whether API.py
@@ -8,9 +8,9 @@ city = input(" Enter the name of the City\n")
 url = f"http://api.weatherapi.com/v1/current.json?key=4adbce37700146d7b3f133836232306&q={city}"
 
 r = requests.get(url)
-# print(r.text)
-wdic = json.loads(r.text)
-print(wdic["current"]["temp_c"])
+print(r.text)
+
 speak = wincom.Dispatch("SAPI.SpVoice")
-text = f"Say 'The Current Whether in {city} is {r} degrees' "
+
+text = f"Say 'The Current Whether in {r.text}' "
 speak.Speak(text)
